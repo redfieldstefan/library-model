3826d605d4f08290aa25a66e8e7ab260654c9fc9
Stefan Redfield <redfield.stefan@gmail.com>
2015-03-15 16:23:11 -0700

    Add jscs, improve linting

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,42 +2,40 @@ module.exports = function(grunt) {
 	'use strict';
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-browserify');
+	grunt.loadNpmTasks('grunt-jscs');
+
+	var srcFiles = ['index.js', 'library.js', 'shelf.js', 'book.js', 'noise.js'];
 
 	grunt.initConfig({
 		jshint: {
-			files: [
-				"q3.js"
-			],
+			files: srcFiles,
 
 			options: {
 				jshintrc: true
 			}
 		},
 
-		browserify: {
-			dev: {
-				src: ['q3.js'],
-				dest: 'bundle.js'
-			}
-		},
-
 		watch: {
-			files: [
-				'q3.js'
-			],
-
+			files: srcFiles,
 			tasks: [
 				'jshint',
-				'browserify:dev'
+				'jscs'
 			]
-		} 
+		},
+
+		jscs: {
+			src: srcFiles,
+
+		    options: {
+		    	config: '.jscsrc'
+		    }				
+    	}
 
 	});
 
 	grunt.registerTask('default', [
 		'jshint',
-		'browserify:dev',
+		'jscs',
 		'watch'
 	]);
 
